429457c972590bc649087580c67469201278fc16
Benjamin Perez <benjamin@bexsoft.net>
2024-09-12 15:34:27 -0600

    test build

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+import {styled} from "@pigment-css/react";
+
+const ButtonBase = styled('button')({
+    padding: '10px 20px',
+    borderRadius: 5,
+    border: 'none',
+    backgroundColor: 'blue',
+    color: 'white',
+    cursor: 'pointer',
+    '&:hover': {
+        backgroundColor: 'darkblue'
+    }
+});
+
+
 export interface ButtonTest {
     sx?: object,
 }
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement & ButtonTest>) {
-    return <button>{props.children}</button>
+    return <ButtonBase>{props.children}</ButtonBase>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@pigment-css/react/styles.css';
 import App from './App.tsx'
 import './index.css'
 
