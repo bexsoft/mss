154afbff5b6fc82e79ea16566c2bda022bd99467
Benjamin Perez <benjamin@bexsoft.net>
2024-09-12 18:19:37 -0600

    test5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import {defineConfig} from 'vite'
-import {extname, relative, resolve} from 'path'
-import {fileURLToPath} from 'node:url'
-import {glob} from 'glob'
+import {resolve} from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import {pigment} from "@pigment-css/vite-plugin";
@@ -25,29 +23,19 @@ export default defineConfig({
         lib: {
             name: "mss",
             entry: resolve(__dirname, 'lib/main.ts'),
-            formats: ["es"],
         },
         rollupOptions: {
+            input: "lib/main.ts",
+            output: [
+                {
+                    esModule: true,
+                    format: "esm",
+                    sourcemap: true,
+                    assetFileNames: 'assets/[name][extname]',
+                    entryFileNames: '[name].js',
+                }
+            ],
             external: ['react', 'react/jsx-runtime'],
-            input: Object.fromEntries(
-                glob.sync('lib/**/**/*.{ts,tsx}', {
-                    ignore: ["lib/**/**/*.d.ts"],
-                }).map(file => [
-                    // The name of the entry point
-                    // lib/nested/foo.ts becomes nested/foo
-                    relative(
-                        'lib',
-                        file.slice(0, file.length - extname(file).length)
-                    ),
-                    // The absolute path to the entry file
-                    // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-                    fileURLToPath(new URL(file, import.meta.url))
-                ])
-            ),
-            output: {
-                assetFileNames: 'assets/[name][extname]',
-                entryFileNames: '[name].js',
-            }
         }
     }
-})
+});
